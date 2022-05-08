567a0e023d9b58155dabeb9775eec80791555217
vocthor <pieceo108@gmail.com>
2022-05-08 14:41:29 +0200

    Program parameters

--- src/main/java/fr/insa/IA/App.java
+++ src/main/java/fr/insa/IA/App.java
@@ -4,48 +4,32 @@ import java.io.IOException;
 import fr.insa.IA.Game.Game;
 import fr.insa.IA.IA.CFR;
 
-
 public class App {
-    
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+        // Pour lancer le programme les paramètres sont " nbIteration JoueurSouhaité(p1
+        // ou p2)"
 
         Game game = new Game(2);
         CFR bot = new CFR();
         game.setBot(bot);
-        try{
-            bot.importMap("modele_serialized_1000.ser");
-        }catch(IOException e){
-            System.out.println("Fichier introuvale, training du bot ...");
-            bot.training(1000);
+        if (args.length != 2) {
+            System.err.println(
+                    "Veuillez lancer le programme avec 2 paramètres : le nombre d'iteration d'entrainement du bot, et les joueurs que vous voulez jouer (p1 ou p2)");
+            System.exit(1);
+        } else {
+            int nbIteration = Integer.parseInt(args[0]);
+            try {
+                bot.importMap("modele_serialized_" + nbIteration + ".ser");
+            } catch (IOException e) {
+                System.out.println("Fichier introuvale, training du bot ...");
+                bot.training(nbIteration);
+            }
+
+            if (args[1].equals("p2")) {
+                game.setCurrentPlayer(game.getPlayerById(2));
+            }
+            game.proceedGame();
         }
-        game.proceedGame();
-      
-        // Pour lancer le programme les paramètres sont " nbIteration JoueurSouhaité(p1 ou p2)"
-        // game.setBot(bot);
-        // if(args.length !=0){
-        //     int nbIteration = Integer.parseInt(args[0]);
-        //     try{
-        //         bot.importMap("modele_serialized_"+nbIteration+".ser");
-        //     }catch(IOException e){
-        //         bot.training(nbIteration);
-        //     }
-        //     if(args.length>1){
-        //         if(args[1].equals("p2")){
-        //             game.setCurrentPlayer(game.getPlayerById(2));
-        //             game.proceedGame();
-        //         }else{
-        //             game.proceedGame();
-        //         }
-        //     }
-        // }else{
-        //     try{
-        //         bot.importMap("modele_serialized_100000.ser");
-        //     }catch(IOException e){
-        //         bot.training(10000);
-        //     }
-        //     game.proceedGame();
-        // }
     }
 }
